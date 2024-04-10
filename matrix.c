2ab1a4eb2083fee1dd3360dfc16e83467db20902
DireRaven22075 <hanhs9803753@gmail.com>
2024-04-10 20:36:05 +0900

    add allocation method

--- matrix.c
+++ matrix.c
@@ -16,5 +16,15 @@ int main (void) {
     scanf("%d %d", &Arows, &Acols);
     printf("B의 행과 열의 개수를 입력하세요: ");
     scanf("%d %d", &Brows, &Bcols);
+
+    //행렬 A, B의 메모리를 동적할당한다.
+    A = (int **)malloc(sizeof(int *) * Arows);
+    for (int i = 0; i < Arows; i++) {
+        A[i] = (int *)malloc(sizeof(int) * Acols);
+    }
+    B = (int **)malloc(sizeof(int *) * Brows);
+    for (int i = 0; i < Brows; i++) {
+        B[i] = (int *)malloc(sizeof(int) * Bcols);
+    }
     return 0;
 }
